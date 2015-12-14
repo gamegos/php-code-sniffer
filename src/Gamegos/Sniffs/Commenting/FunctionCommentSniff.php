f5a95fdfae2ee0548b3c419b9b72ec75856651b1
Safak Ozpinar <safak@gamegos.com>
2015-12-14 18:55:10 +0200

    Add {@inheritdoc} validation for overrided methods.

---
+++ src/Gamegos/Sniffs/Commenting/FunctionCommentSniff.php
@@ -0,0 +1,123 @@
+<?php
+namespace Gamegos\Sniffs\Commenting;
+
+/* Imports from CodeSniffer */
+use PEAR_Sniffs_Commenting_FunctionCommentSniff;
+use PHP_CodeSniffer_File;
+use PHP_CodeSniffer_Tokens;
+use Squiz_Sniffs_Commenting_DocCommentAlignmentSniff;
+
+/**
+ * Customized some rules from PEAR.Commenting.FunctionComment.
+ * - Added {@inheritdoc} validation for overrided methods.
+ * @author Safak Ozpinar <safak@gamegos.com>
+ */
+class FunctionCommentSniff extends PEAR_Sniffs_Commenting_FunctionCommentSniff
+{
+    /**
+     * Check if a comment has a valid {@inheritdoc} annotation.
+     * @param \PHP_CodeSniffer_File $phpcsFile
+     * @param int $stackPtr
+     * @param int $commentStart
+     * @param int $commentEnd
+     */
+    protected function isInheritdoc(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $commentStart, $commentEnd)
+    {
+        $commentString = $phpcsFile->getTokensAsString($commentStart, $commentEnd - $commentStart + 1);
+        if (preg_match('/\{\@inheritdoc\}/', $commentString)) {
+
+            $tokens  = $phpcsFile->getTokens();
+            $nsStart = $phpcsFile->findNext(array(T_NAMESPACE), 0) + 2;
+            $nsEnd   = $phpcsFile->findNext(array(T_SEMICOLON), $nsStart);
+            $class   = '';
+            for ($i = $nsStart; $i < $nsEnd; $i++) {
+                $class .= $tokens[$i]['content'];
+            }
+            $class .= '\\' . $phpcsFile->getDeclarationName($phpcsFile->findNext(array(T_CLASS), $nsEnd));
+
+            if (class_exists($class)) {
+                $method = $phpcsFile->getDeclarationName($stackPtr);
+                foreach (array_merge(class_parents($class), class_implements($class)) as $interface) {
+                    if (method_exists($interface, $method)) {
+                        return true;
+                    }
+                }
+                $error = 'No overrided method found for {@inheritdoc} annotation';
+                $phpcsFile->addError($error, $commentStart, 'InvalidInheritdoc');
+            } else {
+                $warning = 'Cannot validate {@inheritdoc} annotation; class not loaded';
+                $phpcsFile->addWarning($warning, $stackPtr, 'NeedClassLoader');
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $find   = PHP_CodeSniffer_Tokens::$methodPrefixes;
+        $find[] = T_WHITESPACE;
+
+        $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1), null, true);
+        if ($tokens[$commentEnd]['code'] === T_COMMENT) {
+            // Inline comments might just be closing comments for
+            // control structures or functions instead of function comments
+            // using the wrong comment type. If there is other code on the line,
+            // assume they relate to that code.
+            $prev = $phpcsFile->findPrevious($find, ($commentEnd - 1), null, true);
+            if ($prev !== false && $tokens[$prev]['line'] === $tokens[$commentEnd]['line']) {
+                $commentEnd = $prev;
+            }
+        }
+
+        if ($tokens[$commentEnd]['code'] !== T_DOC_COMMENT_CLOSE_TAG
+            && $tokens[$commentEnd]['code'] !== T_COMMENT
+        ) {
+            $phpcsFile->addError('Missing function doc comment', $stackPtr, 'Missing');
+            $phpcsFile->recordMetric($stackPtr, 'Function has doc comment', 'no');
+            return;
+        } else {
+            $phpcsFile->recordMetric($stackPtr, 'Function has doc comment', 'yes');
+        }
+
+        if ($tokens[$commentEnd]['code'] === T_COMMENT) {
+            $phpcsFile->addError('You must use "/**" style comments for a function comment', $stackPtr, 'WrongStyle');
+            return;
+        }
+
+        if ($tokens[$commentEnd]['line'] !== ($tokens[$stackPtr]['line'] - 1)) {
+            $error = 'There must be no blank lines after the function comment';
+            $phpcsFile->addError($error, $commentEnd, 'SpacingAfter');
+        }
+
+        $commentStart = $tokens[$commentEnd]['comment_opener'];
+        foreach ($tokens[$commentStart]['comment_tags'] as $tag) {
+            if ($tokens[$tag]['content'] === '@see') {
+                // Make sure the tag isn't empty.
+                $string = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $tag, $commentEnd);
+                if ($string === false || $tokens[$string]['line'] !== $tokens[$tag]['line']) {
+                    $error = 'Content missing for @see tag in function comment';
+                    $phpcsFile->addError($error, $tag, 'EmptySees');
+                }
+            }
+        }
+
+        $firstOnLine = $phpcsFile->findFirstOnLine(PHP_CodeSniffer_Tokens::$methodPrefixes, $stackPtr);
+        if ($tokens[$commentStart]['column'] !== $tokens[$firstOnLine]['column']) {
+            $error = 'Doc comment is not aligned correctly.';
+            $phpcsFile->addError($error, $commentStart, 'NotAligned');
+            $phpcsFile->removeTokenListener(
+                new Squiz_Sniffs_Commenting_DocCommentAlignmentSniff(), range($commentStart + 1, $commentEnd)
+            );
+            $phpcsFile->refreshTokenListeners();
+        }
+
+        if ($this->isInheritdoc($phpcsFile, $stackPtr, $commentStart, $commentEnd)) {
+            return;
+        }
+        $this->processReturn($phpcsFile, $stackPtr, $commentStart);
+        $this->processThrows($phpcsFile, $stackPtr, $commentStart);
+        $this->processParams($phpcsFile, $stackPtr, $commentStart);
+    }
+}
